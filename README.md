0bffec7380de4424b8de5387e13474b2f87b0897
electrone901 <carbajalluis@rocketmail.com>
2020-12-03 18:43:53 -0500

    added reverse string problem

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # AlgoCasts
 
-Companion repo to [The Coding Inteview Bootcamp: Algorithms + Data Structures](https://www.udemy.com/course/coding-interview-bootcamp-algorithms-and-data-structure/)
+To run all  jest tests:
+=> `jest`
+
+To run one  jest tests:
+=> `jest fib/test.js --watch`
+
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,16 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // using building methods
+  // const arr = str.split('')
+  // return arr.reverse().join('')
+
+  let result = '';
+  for (let character of str) {
+    result = character + result;
+  }
+  return result
+}
 
 module.exports = reverse;
